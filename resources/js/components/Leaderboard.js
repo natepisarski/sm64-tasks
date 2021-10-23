e5bd13d5c2e89c227db36d2480dc654463dcb93a
Nathaniel Pisarski <nathanpisarski@gmail.com>
2021-10-23 08:37:52 -0400

    Don't show the table headings if there are no scores

--- resources/js/components/Leaderboard.js
+++ resources/js/components/Leaderboard.js
@@ -23,11 +23,18 @@ export const Leaderboard = ({leaderboardData}) => {
             </td>
         </tr>
     }
+
+    if (leaderboardData.length === 0) {
+        return <div />
+    }
+
     return <table className={'w-24 text-2xl text-gray-600 mb-6'}>
-        <tr>
-            <th className={'text-center'}>Player</th>
-            <th className={'text-center'}>Score</th>
-        </tr>
+        <thead>
+            <tr>
+                <th className={'text-center'}>Player</th>
+                <th className={'text-center'}>Score</th>
+            </tr>
+        </thead>
         <tbody>
         {leaderboardData.map(data => {
             return getPlayerRow(data.player, data.score);
