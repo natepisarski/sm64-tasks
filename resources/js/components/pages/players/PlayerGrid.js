8c328118bab32b791551bf7eb756fcf6e862a807
Nathaniel Pisarski <nathanpisarski@gmail.com>
2021-11-28 11:00:35 -0500

    Increase the spacing of the name bubbles to make room for long names

--- resources/js/components/pages/players/PlayerGrid.js
+++ resources/js/components/pages/players/PlayerGrid.js
@@ -6,7 +6,7 @@ import {PlayerBubble} from "./PlayerBubble";
 
 export const PlayerGrid = ({players, onPlayerClick}) => {
     return <ul role="list"
-               className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
+               className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-4">
         {players.map(player => <PlayerBubble key={player.id} player={player} onClick={onPlayerClick(player)}/>)}
     </ul>
 };
